113f4ee485416a596ef5d4a1cfe107dc4b612792
Genadij Razdorov <genadijrazdorov@gmail.com>
2017-11-07 10:19:53 +0100

    Used mock decorator in Molecule

--- pybio/molecule.py
+++ pybio/molecule.py
@@ -1,3 +1,6 @@
+from .tools.mock import mock
+
+
 from collections import OrderedDict
 from itertools import groupby
 
@@ -12,10 +15,12 @@ class Molecule(object):
         if notation is not None:
             self.notation = notation
 
+    @mock
     def bind(self, from_, to, bond=None):
         pass
 
     @property
+    @mock
     def formula(self):
         if self.notation == "EEQYNSTYR":
             return Formula("C50H72N14O20")
@@ -27,6 +32,7 @@ class Molecule(object):
             return Formula("C106H164N18O59")
 
     @property
+    @mock
     def mass(self):
         return 2634.545
 
