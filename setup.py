3610371cff0a3bbd3bb1ccb19ab5ae190f12e9cf
Genadij Razdorov <genadijrazdorov@gmail.com>
2017-11-11 17:49:15 +0100

    Fixed for workable sdist (python setup.py sdist)

--- setup.py
+++ setup.py
@@ -3,39 +3,46 @@
 from distutils.core import setup
 import os
 
+import pybio
 
-def find_packages(exclude=None):
-    if exclude is None:
-        exclude = []
+with open("README") as readme:
+    long_description = readme.read()
+
+with open("LICENSE") as lh:
+    license = lh.read()
+
+
+def find_packages(dirs):
     packages = []
-    excluded = []
-    for dirpath, dirnames, filenames in os.walk('.'):
-        if dirpath in exclude:
-            excluded.append(dirpath)
-        elif os.path.dirname(dirpath) in excluded:
-            excluded.append(dirpath)
-        elif "__init__.py" in filenames:
-            packages.append(dirpath)
+    for dirname in dirs:
+        for dirpath, dirnames, filenames in os.walk(dirname):
+            if "__init__.py" not in filenames:
+                continue
+            package = os.path.normpath(dirpath).replace("\\", ".")
+            packages.append(package)
     return packages
 
 setup(
     name='pyBio',
-    version='0.1',
+    version=pybio.__version__,
 
     description='a toolkit for biology related computations',
-    long_description="",    # FIXME
+    long_description=long_description,
 
     author='Genadij Razdorov',
     author_email='genadijrazdorov@gmail.com',
 
     url='https://pybio.readthedocs.io/',
 
-    license='MIT',
+    #license='MIT',
+    license=license,
     
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     #packages=find_packages(exclude=['docs', 'tests']),
-    packages=['pybio'],
+    #packages=find_packages(['pybio', 'tests']),
+    packages=find_packages(['pybio']),
+    #packages=['pybio'],
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
@@ -43,6 +50,8 @@ setup(
     #package_data={
     #    'sample': ['package_data.dat'],
     #},
+
+    data_files=[('', ['README', 'LICENSE', 'CONTRIBUTING'])],
 
     #provides=['pybio'],
 
