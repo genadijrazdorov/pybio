5f84eb91ec84f0f2613a99eae9ef125bf63f184b
Genadij Razdorov <genadijrazdorov@gmail.com>
2017-12-12 09:45:29 +0100

    added requirements

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     #packages=['pybio'],
     
     # requirements
-    requires=['networkx'],
+    #requires=['networkx'],
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
